1f149158031086c0cd7900f060b523d722179d6f
codenor <95989314+codenor@users.noreply.github.com>
2023-08-19 19:23:30 +1000

    Configure tailwind & add README with instructions

---
+++ README.md
@@ -0,0 +1,30 @@
+# bike-shop
+
+A bike shop made with PHP, mySQL and Tailwind CSS.
+
+## Requirements:
+
+- [Docker](https://www.docker.com/)
+- [Tailwind CLI](https://tailwindcss.com/blog/standalone-cli) (or use install script in project if you are using an Unix system)
+
+## Installation/Setup
+
+Compose docker container:
+
+```
+docker-compose up
+```
+
+Run shell script to install and to run the tailwind build process
+
+```
+./run-install.sh
+```
+
+To manually do the tailwind build process:
+
+```
+./tailwindcss -i ./input.css -o ./output.css --watch
+```
+
+Note: You can still run `run-install.sh` instead and it will ignore the install and just do the tailwind build process.
---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Tailwind CSS Test Page</title>
+  <link href="../output.css" rel="stylesheet">
+</head>
+<body class="bg-gray-200">
+  <div class="container mx-auto">
+    <header class="bg-blue-500 text-white p-4">
+      <h1 class="text-4xl">Tailwind CSS Test Page</h1>
+    </header>
+    <main class="p-4">
+      <div class="bg-white p-6 rounded-lg shadow-lg">
+        <h2 class="text-2xl text-gray-700">This is a simple test page.</h2>
+        <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.</p>
+      </div>
+    </main>
+  </div>
+</body>
+</html>
