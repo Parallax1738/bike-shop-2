8343d5f6f433413041f3c4106a35c6bc9154d532
Jed Saxon <jedsaxon@gmail.com>
2023-08-25 15:41:04 +1000

    Now can redirect /home to HomeController (customisable)

---
+++ src/controller/TestController.php
@@ -0,0 +1,10 @@
+<?php
+class TestController extends Controller {
+    public function index() {
+        echo '<h1>Hello World! (test controller)</h1>';
+    }
+    
+    public function ohNo() {
+        echo '<h1>oh fucking shit</h1>';
+    }
+}
--- src/view/index.php
+++ src/view/index.php
@@ -1,21 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Tailwind CSS Test Page</title>
-  <link href="../../output.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <title>Tailwind CSS Test Page</title>
+    <link href="../../output.css" rel="stylesheet">
 </head>
 <body class="bg-gray-200">
-  <div class="container mx-auto">
+<div class="container mx-auto">
     <header class="bg-blue-500 text-white p-4">
-      <h1 class="text-4xl">Tailwind CSS Test Page</h1>
+        <h1 class="text-4xl">Tailwind CSS Test Page</h1>
     </header>
     <main class="p-4">
-      <div class="bg-white p-6 rounded-lg shadow-lg">
-        <h2 class="text-2xl text-gray-700">This is a simple test page.</h2>
-        <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.</p>
-      </div>
+        <?php
+            
+            require '../base/MvcUri.php';
+            require '../base/Router.php';
+            
+            /**
+             * Loop through URL's string. Add all characters to $str. When / is found, save that string to find get
+             * the controller/action. Once ? is found, add to parameters array. Therefore, we should loop through
+             * the string's characters and break every time we find a / or a ?. This is a fucking awful 'solution'
+             * @return MvcUri The Uri as an object to get whatever the fuck the user entered as an object
+            */
+            function getUri(): MvcUri
+            {
+                $url = $_SERVER["REQUEST_URI"];
+                
+                $controller = "";
+                $action = "";
+                $params = [];
+                $tempString = "";
+                
+                // | http://example.com | OR | http://example.com/ |
+                if ($url == "" || $url == "/") {
+                    return new MvcUri($controller, $action, $params);
+                }
+                
+                $urlSplit = mb_str_split($url);
+                $urlSplit = array_slice($urlSplit, 1); // Get rid of the first '/' to not break things :(
+                
+                foreach ($urlSplit as $char) {
+                    // If neither controller nor action is empty and there is a /, then add it to controller or action
+                    if (!empty($controller) && !empty($action)) {
+                        // do parameters
+                    }
+                    else if ($char == "/") {
+                        // If the controller is empty, then we know that it isn't set, and that we must do it now.
+                        if (empty($controller)) {
+                            $controller = $tempString;
+                            $tempString = "";
+                        }
+                    }
+                    else if ($char == "?" && empty($action)) {
+                        // The action usually becomes before parameters via '?', for example:
+                        // http://example.com/controller/"action?test=5"
+                        $action = $tempString;
+                        $tempString = "";
+                    }
+                    else {
+                        $tempString .= $char;
+                    }
+                }
+                if (empty($controller)) {
+                    $controller = $tempString;
+                } else if (empty($action)) {
+                    $action = $tempString;
+                }
+                
+                return new MvcUri($controller, $action, $params);
+            }
+            
+            $router = new Router();
+            $router->manageUrl(getUri());
+            
+            getUri();
+        ?>
     </main>
-  </div>
+</div>
 </body>
 </html>
