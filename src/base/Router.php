e350768d4097fd25a5cde9ef88377cff9ceb2440
Jed Saxon <jedsaxon@gmail.com>
2023-09-21 11:24:33 +1000

    Moved getUri() from Index.php to Router.php

--- src/base/Router.php
+++ src/base/Router.php
@@ -22,8 +22,10 @@
 			$this->controllerMap[ "auth" ] = new AuthController();
 		}
 		
-		public function manageUrl(MvcUri $uri) : void
+		public function manageUrl() : void
 		{
+			$uri = $this->getUri();
+			
 			// If no controller, go to /
 			if (empty($uri->getControllerName())) {
 				$this->displayIndex($uri->getParametersArray());
@@ -63,6 +65,57 @@
 			} catch (Exception $exception) {
 				$this->notFound("For fucks sake the home controller doesn't have an index method: " . $exception);
 			}
+		}
+		
+		/**
+		 * Loop through URL's string. Add all characters to $str. When / is found, save that string to find get
+		 * the controller/action. Once ? is found, add to parameters array. Therefore, we should loop through
+		 * the string's characters and break every time we find a / or a ?. This is a fucking awful 'solution'
+		 * @return MvcUri The Uri as an object to get whatever the fuck the user entered as an object
+		 */
+		private function getUri() : MvcUri
+		{
+			$url = $_SERVER[ "REQUEST_URI" ];
+			
+			$controller = "";
+			$action = "";
+			$params = [];
+			$tempString = "";
+			
+			// | http://example.com | OR | http://example.com/ |
+			if ($url == "" || $url == "/") {
+				return new MvcUri($controller, $action, $params);
+			}
+			
+			$urlSplit = mb_str_split($url);
+			$urlSplit = array_slice($urlSplit, 1); // Get rid of the first '/' to not break things :(
+			
+			foreach ($urlSplit as $char) {
+				// If neither controller nor action is empty and there is a /, then add it to controller or action
+				if (!empty($controller) && !empty($action)) {
+					// do parameters
+				} else if ($char == "/") {
+					// If the controller is empty, then we know that it isn't set, and that we must do it now.
+					if (empty($controller)) {
+						$controller = $tempString;
+						$tempString = "";
+					}
+				} else if ($char == "?" && empty($action)) {
+					// The action usually becomes before parameters via '?', for example:
+					// http://example.com/controller/"action?test=5"
+					$action = $tempString;
+					$tempString = "";
+				} else {
+					$tempString .= $char;
+				}
+			}
+			if (empty($controller)) {
+				$controller = $tempString;
+			} else if (empty($action)) {
+				$action = $tempString;
+			}
+			
+			return new MvcUri($controller, $action, $params);
 		}
 		
 		private function notFound($message) : void
