66f93a28f57923a828611ad91b817a5e1693d692
Jed Saxon <jedsaxon@gmail.com>
2023-09-23 13:54:10 +1000

    Made $data parameter for Controller->view() optional

--- src/base/Controller.php
+++ src/base/Controller.php
@@ -2,11 +2,12 @@
 	
 	class Controller
 	{
-        protected function view(string $controller, string $action, $data): void
+		
+        protected function view(string $controller, string $action, $data = null): void
         {
             $fileName = __DIR__ . "/../view/" . $controller . "/" . $action . ".php";
             if (!file_exists($fileName)) {
-                $this->viewError("The view does not exist, dumbass. Make sure you know how the fuck to type ");
+                $this->viewError("The view {" . $fileName ."} does not exist, dumbass. Make sure you know how the fuck to type ");
             } else {
 				// Loads the file, which is where the HTML/CSS will be loaded for the user
                 require_once $fileName;
