3d31f10ae2d0b19b2b4757f4f2ca7d43e0961bb0
codenor <95989314+codenor@users.noreply.github.com>
2023-10-11 18:52:33 +1100

    Edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/{core,controller,view}/**/*.php',
-  ],
+  content: ["./src/**/*.{php,html}"],
   theme: {
     extend: {},
   },
