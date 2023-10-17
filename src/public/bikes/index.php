f7d2a1fde873210d803571b41cc9cfcd76ec8e00
Diseased Finger <jedsaxon@gmail.com>
2023-10-17 20:00:00 +1100

    Fixde issue where manually inserting HTTP params didn't work on products view

--- src/public/bikes/index.php
+++ src/public/bikes/index.php
@@ -20,31 +20,25 @@
 	{
 		// Display Left Arrow because if it is greater than 0, user can go back a page
 		$newPage = $this->currentPage - 1;
-		echo '<form method="get" action="http://localhost/bikes?page=' . $newPage . '&results=' . $data->getMaxResults() . '">
-					<input type="submit" value="_<_" style="background-color: darkgrey" />
-				</form>';
+		
+		echo '<form method="get" action="/bikes">
+			<input type="hidden" name="page" value="' . $newPage . '" />
+			<input type="hidden" name="results" value="' . $data->getMaxResults() . '" />
+			<input type="submit" value="_>_" style="background-color: darkgrey" />
+			</form>';
 	}
 	
 	echo "<div style='background-color: orange'>" . $data->getCurrentPage() + 1 . " / " . $data->getMaxPage();
-	
-//	// Next Button
-//	if ($data->getCurrentPage() < $data->getMaxPage() - 1)
-//	{
-//		// Oposite to if statement above
-//		$newPage = $data->getCurrentPage() + 1;
-//		echo '<form method="get" action="http://localhost/bikes?page=' . $newPage . '&results=' . $data->getMaxResults() . '">
-//					<input type="submit" value="_>_" style="background-color: darkgrey"/>
-//				</form>';
-//	}
 	
 	if ($data->getCurrentPage() < $data->getMaxPage() - 1)
 	{
 		$newPage = $data->getCurrentPage() + 1;
 		
-		echo '<form method="get" action="/test">
-				<input name="page"
-					<input type="submit" value="_>_" style="background-color: darkgrey" />
-				</form>';
+		echo '<form method="get" action="/bikes">
+			<input type="hidden" name="page" value="' . $newPage . '" />
+			<input type="hidden" name="results" value="' . $data->getMaxResults() . '" />
+			<input type="submit" value="_>_" style="background-color: darkgrey" />
+			</form>';
 	}
-
+	
 ?>
