59f3677250a0107645c84d948aedeb0aca76eeb4
Diseased Finger <jedsaxon@gmail.com>
2023-11-10 16:10:56 +1100

    Made 'total price' actually usable

--- src/public/cart/index.php
+++ src/public/cart/index.php
@@ -31,12 +31,13 @@
             <tbody>';
         foreach ($data->getProducts() as $p)
         {
+            // Info - Uses javascript to load the quantities
             if ($p instanceof ProductEntity)
             {
                 echo '<tr>';
                 echo '<td>' . $p->getName() . '</td>';
                 echo '<td>$' . $p->getPrice() . '</td>';
-                echo '<td><input class="quantity" min=0 data-id="'.$p->getId().'" type="number" value="-1" step="1"/></td>';
+                echo '<td><input class="quantity" min=0 data-id="'.$p->getId().'" data-price="'.$p->getPrice().'" type="number" value="-1" step="1"/></td>';
                 echo '<td><a href="/products/details?product=' . $p->getId() . '" style="text-decoration: underline; color: blue;">View Details</a></td>';
                 echo '<tr>';
             }
@@ -44,31 +45,43 @@
         echo '
             </tbody>
             </table>
-            <br><hr><br><p>Total: $' . $total . '</p>
+            <br><hr><br><p id="total">Total: $' . $total . '</p>
             <a style="color: blue; text-decoration: underline">Purchase</a>';
     }
 	?>
 <script>
 	let inputs = document.querySelectorAll('.quantity')
+    let totalText = document.querySelector('.total');
     for (let i = 0; i < inputs.length; i++)
     {
         inputs[i].addEventListener("input", (event) => {
             let quantity = event.target.value;
             let productId = event.target.dataset.id;
-            changeQuantityInCart(Number(productId), Number(quantity)).then();
+            changeQuantityInCart(Number(productId), Number(quantity)).then(() => {
+                // Once the quantity changes in the cookie, reload teh prices
+                initQuantities();
+            });
         })
     }
     initQuantities();
     
     function initQuantities() {
         getCart().then((cart) => {
+            let total = 0;
             let inputs = document.querySelectorAll('.quantity');
+            let totalText = document.querySelector('#total');
             for (let i = 0; i < inputs.length; i++) {
                 let foundProductId = findProductInCart(cart, inputs[i].dataset.id);
                 if (foundProductId === -1) continue;
-                // changeQuantity(inputs[i].dataset.id, cart[i]['q']);
-                inputs[i].value = cart[i]['q'];
+                let price = inputs[i].dataset.price ?? 0;
+                
+                // Set Quantity
+                let q = cart[i]['q'];
+                inputs[i].value = q;
+                
+                total += price * q;
             }
+            totalText.innerHTML = "Total: $" + total.toFixed(2);
         });
     }
 </script>
